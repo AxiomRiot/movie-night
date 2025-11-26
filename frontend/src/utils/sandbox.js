c9216abccbec4155501ad6ff126fdf1f3e106273
Zachery Knopp <zacheryknopp@gmail.com>
2025-11-25 20:57:26 -0500

    Added backend server

--- frontend/src/utils/sandbox.js
+++ frontend/src/utils/sandbox.js
@@ -1,6 +1,10 @@
+import { readFile } from 'node:fs/promises';
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync({ sigint: true });
 const API_ACCESS_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTMyNWY2NzlhMGVlYjQ3M2ZjYTJhZDM1ZmVkMjdlNCIsIm5iZiI6MTc1ODc2MTY2My4zNTIsInN1YiI6IjY4ZDQ5MmJmODM4N2FkYzZjOTkxMzkxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z-aSlS1E83jzYdGMnJQptk1KUqkj3Uyg5_cUHTJMrLA';
 
-async function fetchData() {
+async function fetchData(url) {
   const options = {
     method: 'GET',
     headers: {
@@ -9,28 +13,33 @@ async function fetchData() {
     },
   };
 
-  // const url = `https://api.themoviedb.org/3/account`;
-  // const url = 'https://api.themoviedb.org/3/search/movie?query=Aliens&include_adult=false&language=en-US&page=1';
-  const url = 'https://api.themoviedb.org/3/movie/679?language=en-US';
-
   const response = await fetch(url, options);
-  const data = await response.json();
-
-  console.warn(data);
+  return await response.json();
 }
 
-async function postData() {
+async function searchForMovie(movie) {
+  const url = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(movie)}&include_adult=true&language=en-US&page=1`;
+  return await fetchData(url);
+}
+
+async function getWatchlist() {
+  const url = 'https://api.themoviedb.org/3/account/22335753/watchlist/movies';
+  const data = await fetchData(url);
+  return (data.results || []).map((m) => m.title);
+}
+
+async function addMovieToWatchlist(movieTitle, movieId) {
   const url = 'https://api.themoviedb.org/3/account/22335753/watchlist';
   const options = {
     method: 'POST',
     headers: {
-      'accept': 'application/json',
+      accept: 'application/json',
       'content-type': 'application/json',
-      'Authorization': `Bearer ${API_ACCESS_KEY}`,
+      Authorization: `Bearer ${API_ACCESS_KEY}`,
     },
     body: JSON.stringify({
       media_type: 'movie',
-      media_id: 679,
+      media_id: movieId,
       watchlist: true,
     }),
   };
@@ -38,7 +47,50 @@ async function postData() {
   const response = await fetch(url, options);
   const data = await response.json();
 
-  console.warn(data);
+  if (data.status_code === 1) {
+    console.warn(`Successfully added ${movieTitle} to watch list`);
+  } else {
+    console.error(`Failed to add ${movieTitle} to watchlist: ${data.status_message}`);
+  }
 }
 
-postData();
+async function main() {
+  try {
+    const watchList = await getWatchlist();
+    console.warn(watchList);
+    
+    // use promise-based readFile
+    const csv = await readFile('movie-list.csv', 'utf8');
+    const rows = csv.split('\n');
+    const movieList = [];
+
+    for (let i = 0; i < rows.length; i++) {
+      const values = rows[i].split(',');
+      movieList.push(values[0]);
+    }
+
+    for (let i = 0; i < movieList.length; i++) {
+      const movieTitle = movieList[i];
+
+      if (!watchList.includes(movieTitle)) {
+        console.warn('---------------------------------------------');
+        const data = await searchForMovie(movieTitle);
+        data.results.forEach((movie, index) => {
+          const selectionLine = `${index}. ${movie.original_title}: ${movie.overview}`;
+          console.warn(selectionLine);
+        });
+        const userInput = prompt('Please select a movie to add to wishlist: ', '0');
+        const selectedMovie = data.results[Number(userInput)];
+        await addMovieToWatchlist(selectedMovie.title, selectedMovie.id, watchList);
+      }
+      else {
+        console.warn(`${movieTitle} already exists in watchlist!`);
+      }
+    }
+  }
+  catch (err) {
+    console.error('Error in main:', err);
+  }
+}
+
+main();
