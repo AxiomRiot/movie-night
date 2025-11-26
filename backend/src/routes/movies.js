052b714283651678c64c9985531b6a3cce793876
Zachery Knopp <zacheryknopp@gmail.com>
2025-11-25 21:28:41 -0500

    Moved backend to separate repo

--- backend/src/routes/movies.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getRandomMovieController } = require('../controllers/movieController');
-const router = new express.Router();
-
-router.get('/movie/', getRandomMovieController);
-
-module.exports = router;
